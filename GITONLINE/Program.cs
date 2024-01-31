383e0f0710ae7a7f221ced8c7b09fcbce3d97d1a
angel <a2308744@institutmontilivi.cat>
2024-01-31 19:50:52 +0100

    asdfsd

--- GITONLINE/Program.cs
+++ GITONLINE/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, montilivi!");
+            Console.WriteLine("Hello, montilivi2!");
         }
     }
 }
